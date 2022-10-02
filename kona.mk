a10f162c4fe47ba7a5f7d085e4178534a6d71739
johnmart19 <johnivan19999@gmail.com>
2022-10-02 19:27:39 +0300

    sm8250-common: Audio: Build android.hardware.audio@7.1-util
    - Matching stock environment
    - Also provies compilation of:
    * android.hardware.audio@7.1
    * android.hardware.audio.common@7.0
    * android.hardware.audio.common@7.1-enums
    * android.hardware.audio.common@7.1-util
    
    Currently in system/lib we had only android.hardware.audio.common@2.0 & @5.0
    In system/lib64 we had only android.hardware.audio.common@2.0.so @4.0 @4.0-util @5.0 @5.0-util @6.0 @6.0-util and AIDL

--- kona.mk
+++ kona.mk
@@ -103,6 +103,7 @@ endif
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.0-impl \
