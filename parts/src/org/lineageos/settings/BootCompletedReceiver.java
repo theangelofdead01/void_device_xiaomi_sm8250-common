0ed6bd954b9b2ef4b9e68cd420fc652a43ed3582
johnmart19 <johnivan19999@gmail.com>
2023-02-22 15:21:41 +0200

    sm8250-common: Drop support of HBM
    - No longer supported and will not work
    
    This reverts commits:
    - 3f098f69f5b920ef9a0b0499777cb87c3502a193
    - 2e255d56775834ff6c2917017e06c1d9c2fcd6ec

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,7 +27,6 @@ import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
-import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -46,6 +45,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
-        FileUtils.enableService(context);
     }
 }
