6a312d7f75c51061c965f0c566f7e2bd2beb9081
theangelofdead01 <rockinthecity142@gmail.com>
2023-06-15 20:12:26 -0500

    sm8250-common: fix bootloop about sensors libs

--- kona.mk
+++ kona.mk
@@ -499,7 +499,10 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl-xiaomi \
+    android.hardware.sensors@1.0-impl \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-service
 
 PRODUCT_PACKAGES += \
